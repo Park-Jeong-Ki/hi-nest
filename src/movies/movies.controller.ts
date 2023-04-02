921a550e2181ae3edc136b4383022eaad68e45e1
Park-Jeong-Ki <jgpark1998@gmail.com>
2023-04-02 18:18:33 +0900

    3.0

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,6 +1,8 @@
 import { Controller,Get,Param,Delete,Post,Patch,Body,Query } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entity';
+import { CreateMovieDTO } from './dto/create-movie.dto';
+import { UpdateMovieDTO } from './dto/update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -14,27 +16,27 @@ export class MoviesController {
   }
   
   // @Get("search")
-  // search(@Query("year") searchingYear: string){
+  // search(@Query("year") searchingYear: number){
   //   return `We are searching for a movie made after: ${searchingYear}`;
   // }
 
   @Get(":id")
-  getOne(@Param("id") movieId:string) : Movie {
+  getOne(@Param("id") movieId:number) : Movie {
     return this.moviesService.getOne(movieId);
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDTO) {
     return this.moviesService.create(movieData);
-    }
+  }
   
   @Delete()
-  remove(@Param('id') movieId:string) {
+  remove(@Param('id') movieId:number) {
     return this.moviesService.deleteOne(movieId);
   }
 
   @Patch(':id')
-  path(@Param('id') movieId: string, @Body() upateData) {
+  path(@Param('id') movieId: number, @Body() upateData: UpdateMovieDTO) {
     return this.moviesService.update(movieId, upateData);
   }
 
